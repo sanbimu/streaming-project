f93ec64fe78334bf929530b6951ac736a51291f1
Loïc Francotte <loicfrancotte@MBP-de-Loic.lan>
2023-02-13 16:44:30 +0100

    modif backend

--- backend/server.js
+++ backend/server.js
@@ -33,10 +33,6 @@ let corsOptions = {
   origin: 'http://localhost:8081'
 };
 
-app.use(cors(corsOptions));
-
-app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
