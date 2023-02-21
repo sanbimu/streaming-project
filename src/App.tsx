88984f0d5fa4497a5f9153317dbfa36efe2d393c
sanbimu <119463187+sanbimu@users.noreply.github.com>
2023-02-21 09:36:03 +0100

    payment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import LandingPage from './pages/LandingPage';
